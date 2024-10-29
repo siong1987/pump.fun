acfc47e3968426cd24b7cd933bbc410e0840c2c6
Siong Ong <siong1987@gmail.com>
2024-10-29 23:28:02 +0800

    pool creation should create mint and amount

--- programs/bonding_curve/src/instructions/create_pool.rs
+++ programs/bonding_curve/src/instructions/create_pool.rs
@@ -2,15 +2,51 @@ use crate::state::*;
 use anchor_lang::prelude::*;
 use anchor_spl::{
     associated_token::AssociatedToken,
-    token::{Mint, Token, TokenAccount},
+    token::{mint_to, set_authority, Mint, MintTo, SetAuthority, Token, TokenAccount},
 };
 
 pub fn create_pool(ctx: Context<CreateLiquidityPool>) -> Result<()> {
     let pool = &mut ctx.accounts.pool;
+    let pool_mint_key = ctx.accounts.pool_mint.key();
+    let pool_bump_seed = ctx.bumps.pool;
+    let pool_signer_seeds: &[&[&[u8]]] = &[&[
+        LiquidityPool::POOL_SEED_PREFIX.as_bytes(),
+        pool_mint_key.as_ref(),
+        &[pool_bump_seed],
+    ]];
+
+    let token_program = &ctx.accounts.token_program;
+
+    // Mint tokens to the pool token account
+    let mint_cpi_ctx = CpiContext::new_with_signer(
+        token_program.to_account_info(),
+        MintTo {
+            mint: ctx.accounts.pool_mint.to_account_info(),
+            to: ctx.accounts.pool_token_account.to_account_info(),
+            authority: pool.to_account_info(),
+        },
+        pool_signer_seeds,
+    );
+    mint_to(mint_cpi_ctx, 10000000000000)?; // TODO: check how much
+
+    // Give up the mint authority
+    let give_up_authority_ctx = CpiContext::new_with_signer(
+        token_program.to_account_info(),
+        SetAuthority {
+            current_authority: pool.to_account_info(),
+            account_or_mint: ctx.accounts.pool_mint.to_account_info(),
+        },
+        pool_signer_seeds,
+    );
+    set_authority(
+        give_up_authority_ctx,
+        spl_token::instruction::AuthorityType::MintTokens,
+        None,
+    )?;
 
     pool.set_inner(LiquidityPool::new(
         ctx.accounts.payer.key(),
-        ctx.accounts.token_mint.key(),
+        ctx.accounts.pool_mint.key(),
         ctx.bumps.pool,
     ));
     Ok(())
@@ -22,18 +58,23 @@ pub struct CreateLiquidityPool<'info> {
         init,
         space = LiquidityPool::ACCOUNT_SIZE,
         payer = payer,
-        seeds = [LiquidityPool::POOL_SEED_PREFIX.as_bytes(), token_mint.key().as_ref()],
+        seeds = [LiquidityPool::POOL_SEED_PREFIX.as_bytes(), pool_mint.key().as_ref()],
         bump
     )]
     pub pool: Box<Account<'info, LiquidityPool>>,
 
-    #[account(mut)]
-    pub token_mint: Box<Account<'info, Mint>>,
+    #[account(
+        init,
+        payer = payer,
+        mint::decimals = 9, // TODO: check how many
+        mint::authority = pool,
+    )]
+    pub pool_mint: Account<'info, Mint>,
 
     #[account(
         init,
         payer = payer,
-        associated_token::mint = token_mint,
+        associated_token::mint = pool_mint,
         associated_token::authority = pool
     )]
     pub pool_token_account: Box<Account<'info, TokenAccount>>,
@@ -42,6 +83,5 @@ pub struct CreateLiquidityPool<'info> {
     pub payer: Signer<'info>,
     pub token_program: Program<'info, Token>,
     pub associated_token_program: Program<'info, AssociatedToken>,
-    pub rent: Sysvar<'info, Rent>,
     pub system_program: Program<'info, System>,
 }
--- tests/bonding-curve.ts
+++ tests/bonding-curve.ts
@@ -16,17 +16,16 @@ import {
   getOrCreateAssociatedTokenAccount,
   mintTo,
   getAssociatedTokenAddress,
+  getMint,
 } from "@solana/spl-token";
 import { expect } from "chai";
 import { BN } from "bn.js";
-import keys from "../keys/users.json";
-import key2 from "../keys/user2.json";
+import userSecretKey from "../keys/user.json";
+import mintSecretKey from "../keys/mint.json";
 import {
   ASSOCIATED_PROGRAM_ID,
   TOKEN_PROGRAM_ID,
 } from "@coral-xyz/anchor/dist/cjs/utils/token";
-
-const connection = new Connection("http://localhost:8899");
 const curveSeed = "CurveConfiguration";
 const POOL_SEED_PREFIX = "liquidity_pool";
 const LIQUIDITY_SEED = "LiqudityProvider";
@@ -40,15 +39,13 @@ describe("bonding_curve", () => {
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.BondingCurve as Program<BondingCurve>;
-
-  // custom setting
-  const user = Keypair.fromSecretKey(new Uint8Array(keys));
-  const user2 = Keypair.fromSecretKey(new Uint8Array(key2));
+  const connection = program.provider.connection;
+  const user = Keypair.fromSecretKey(new Uint8Array(userSecretKey));
+  const poolMint = Keypair.fromSecretKey(new Uint8Array(mintSecretKey));
   const tokenDecimal = 9;
   const amount = new BN(1000000000).mul(new BN(10 ** tokenDecimal));
 
-  let mint1: PublicKey;
-  let tokenAta1: PublicKey;
+  // let tokenAta1: PublicKey;
 
   // let mint2: PublicKey
   // let tokenAta2: PublicKey
@@ -99,47 +96,47 @@ describe("bonding_curve", () => {
   //   console.log("user balance : ", (await connection.getBalance(user.publicKey)) / 10 ** 9, "SOL")
   // })
 
-  it("Mint token1 to user wallet", async () => {
-    console.log("Trying to create and mint token1 to user's wallet");
+  // it("Mint token1 to user wallet", async () => {
+  //   console.log("Trying to create and mint token1 to user's wallet");
 
-    try {
-      mint1 = await createMint(
-        connection,
-        user,
-        user.publicKey,
-        user.publicKey,
-        tokenDecimal
-      );
-      console.log("mint1 address: ", mint1.toBase58());
-      tokenAta1 = (
-        await getOrCreateAssociatedTokenAccount(
-          connection,
-          user,
-          mint1,
-          user.publicKey
-        )
-      ).address;
-      console.log("token1 account address: ", tokenAta1.toBase58());
-      try {
-        //minting 100 new tokens to the token address we just created
-        await mintTo(
-          connection,
-          user,
-          mint1,
-          tokenAta1,
-          user.publicKey,
-          BigInt(amount.toString())
-        );
-      } catch (error) {
-        console.log("🚀 ~ here:", error);
-      }
-      const tokenBalance = await connection.getTokenAccountBalance(tokenAta1);
-      console.log("tokenBalance1 in user:", tokenBalance.value.uiAmount);
-      console.log("token 1 successfully minted");
-    } catch (error) {
-      console.log("Token 1 creation error \n", error);
-    }
-  });
+  //   try {
+  //     mint1 = await createMint(
+  //       connection,
+  //       user,
+  //       user.publicKey,
+  //       user.publicKey,
+  //       tokenDecimal
+  //     );
+  //     console.log("mint1 address: ", mint1.toBase58());
+  //     tokenAta1 = (
+  //       await getOrCreateAssociatedTokenAccount(
+  //         connection,
+  //         user,
+  //         mint1,
+  //         user.publicKey
+  //       )
+  //     ).address;
+  //     console.log("token1 account address: ", tokenAta1.toBase58());
+  //     try {
+  //       //minting 100 new tokens to the token address we just created
+  //       await mintTo(
+  //         connection,
+  //         user,
+  //         mint1,
+  //         tokenAta1,
+  //         user.publicKey,
+  //         BigInt(amount.toString())
+  //       );
+  //     } catch (error) {
+  //       console.log("🚀 ~ here:", error);
+  //     }
+  //     const tokenBalance = await connection.getTokenAccountBalance(tokenAta1);
+  //     console.log("tokenBalance1 in user:", tokenBalance.value.uiAmount);
+  //     console.log("token 1 successfully minted");
+  //   } catch (error) {
+  //     console.log("Token 1 creation error \n", error);
+  //   }
+  // });
 
   // it("Mint token 2 to user wallet", async () => {
   //   console.log("Trying to create and mint token 2 to user's wallet")
@@ -175,46 +172,54 @@ describe("bonding_curve", () => {
       .signers([user])
       .rpc();
 
-    let curveConfiguration = await program.account.curveConfiguration.fetch(
+    const curveConfiguration = await program.account.curveConfiguration.fetch(
       curveConfig
     );
     console.log("Curve configuration: ", curveConfiguration);
   });
 
-  it("create pool", async () => {
+  it("creates pool", async () => {
     try {
-      const [poolPda] = PublicKey.findProgramAddressSync(
-        [Buffer.from(POOL_SEED_PREFIX), mint1.toBuffer()],
+      const [pool] = PublicKey.findProgramAddressSync(
+        [Buffer.from(POOL_SEED_PREFIX), poolMint.publicKey.toBuffer()],
         program.programId
       );
-      const poolToken = await getAssociatedTokenAddress(mint1, poolPda, true);
-      const tx = new Transaction().add(
-        ComputeBudgetProgram.setComputeUnitLimit({ units: 200_000 }),
-        ComputeBudgetProgram.setComputeUnitPrice({ microLamports: 200_000 }),
-        await program.methods
-          .createPool()
-          .accounts({
-            pool: poolPda,
-            tokenMint: mint1,
-            poolTokenAccount: poolToken,
-            payer: user.publicKey,
-            tokenProgram: TOKEN_PROGRAM_ID,
-            rent: SYSVAR_RENT_PUBKEY,
-            associatedTokenProgram: ASSOCIATED_PROGRAM_ID,
-            systemProgram: SystemProgram.programId,
-          })
-          .instruction()
+      const poolToken = await getAssociatedTokenAddress(
+        poolMint.publicKey,
+        pool,
+        true
       );
-      tx.feePayer = user.publicKey;
-      tx.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
-      console.log(await connection.simulateTransaction(tx));
-      const sig = await sendAndConfirmTransaction(connection, tx, [user], {
-        skipPreflight: true,
-      });
-      console.log(
-        "Successfully created pool : ",
-        `https://solscan.io/tx/${sig}?cluster=devnet`
+      await program.methods
+        .createPool()
+        .accounts({
+          pool,
+          poolMint: poolMint.publicKey,
+          poolTokenAccount: poolToken,
+          payer: user.publicKey,
+          tokenProgram: TOKEN_PROGRAM_ID,
+          associatedTokenProgram: ASSOCIATED_PROGRAM_ID,
+          systemProgram: SystemProgram.programId,
+        })
+        .signers([user, poolMint])
+        .rpc();
+
+      const liquidityPool = await program.account.liquidityPool.fetch(pool);
+      console.log("Liquidity Pool: ", liquidityPool);
+
+      const mintAccount = await getMint(connection, poolMint.publicKey);
+      console.log("Mint: ", mintAccount);
+      expect(mintAccount.address.toString()).to.eq(
+        poolMint.publicKey.toString()
       );
+      expect(mintAccount.decimals).to.eq(9);
+      expect(mintAccount.freezeAuthority).to.be.null;
+      expect(mintAccount.mintAuthority).to.be.null;
+
+      const poolTokenAccount = await connection.getTokenAccountBalance(
+        poolToken
+      );
+      console.log("Pool token account: ", poolTokenAccount);
+      expect(poolTokenAccount.value).to.eq("10000000000000");
     } catch (error) {
       console.log("Error in creating pool", error);
     }
